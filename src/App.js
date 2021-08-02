afd2e6f0a2b88fc891440e44015a2bff4e146f98
abroccoli <acbrocco@syr.edu>
2021-08-02 14:54:31 -0400

    rm banner for now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import {ScheduleTable} from './components/ScheduleTable/ScheduleTable';
-import SthBanner from './bruins_sth_banner.png';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-        <img className="sth-banner" src={SthBanner}>
-        </img>
         <div className='text-block'>
             <h1>Boston Bruins Tickets</h1>
             <h3>Directly from a Bruins season ticket holder.</h3>
             <h3>Below box office value. No fees. No BS.</h3>
         </div>
         <div className='text-block text-block--spacer'>
-        <div>
-            Text ‪<a href='tel‪:+16177024741‬'>(617) 702-4741‬</a> with the games you are interested in purchasing.
+            <div>
+                Text ‪<a href='tel‪:+16177024741‬'>(617) 702-4741‬</a> with the games you are interested in purchasing.
+            </div>
+            <div>
+                All payments will be made via PayPal.
+            </div>
+            <div>
+            Ticketmaster SafeTix will be transfered to your Ticketmaster account after payment is recieved.
+            </div>
+            <div>
+                All prices listed are for both seats.
+            </div>
+            <div>
+                Seats will not be sold individually.
+            </div>
         </div>
-        <div>
-            All payments will be made via PayPal. Ticketmaster SafeTix will be transfered to your Ticketmaster account after payment is recieved.
-        </div>
+        <div className='text-block text-block--spacer'>
+            <h3> 
+                Section 306, Row 7, Seats 3 &amp; 4
+            </h3>
         </div>
         <div className='text-block text-block--spacer'>
             Boston Bruins 2021-2022 Regular Season
