ae4f23114a3de43971cc31813594adb7d971b758
abroccoli <acbroccoli@gmail.com>
2024-10-11 12:11:19 -0400

    more games

--- src/constants/Schedule2024.js
+++ src/constants/Schedule2024.js
@@ -1,5 +1,23 @@
 const Schedule2024November = [
     {
+        Date: 'November 7th, 2024',
+        Time: '7:00 PM',
+        Day: 'Thursday',
+        Opponent: 'Calgary Flames',
+        Subtext: false,
+        Price: 90,
+        IsAvailable: true
+    },
+    {
+        Date: 'November 16th, 2024',
+        Time: '1:00 PM',
+        Day: 'Saturday',
+        Opponent: 'St. Louis Blues',
+        Subtext: false,
+        Price: 115,
+        IsAvailable: true
+    },
+    {
         Date: 'November 18th, 2024',
         Time: '7:00 PM',
         Day: 'Monday',
